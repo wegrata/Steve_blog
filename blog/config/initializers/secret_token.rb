b7c36c2b03d72a63a73d67cbfa2031b5edfd8865
Stephen Trevorrow <strevorrow@problemsolutions.net>
2012-06-20 14:52:20 -0400

    Created blog skeleton. Allows blog posts, and comments. Deletion of comments/blog posts. Editting blog posts.

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'e8321e315b7bd567c7d4cdc0247b6a5f36659805ec2cae043c0014278800bc96811862593812501d59fc3b2ace37152984f8e3c7fbbf26a395f57d071abb0b00'
